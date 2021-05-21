a379f38df8ee8cb5ef1cf61989111776e44af04f
Robert <787751565@qq.com>
2021-05-21 11:47:44 +0800

    Update getCodeRunTimeAndUsage.php

--- PHP/计算程序运行时间及内存消耗/getCodeRunTimeAndUsage.php
+++ PHP/计算程序运行时间及内存消耗/getCodeRunTimeAndUsage.php
@@ -1,11 +1,11 @@
 <?php
 
 // 记录开始时间
-$starttime = getMicrotime();
+$starttime = microtime(true);
 
 // 记录结束时间
-$endtime = getMicrotime();
+$endtime = microtime(true);
 
 // 计算程序的耗时及消耗的内存
-echo '耗时:'.(float)(($endtime-$starttime)*1000).'ms<br>';
+echo '耗时' . round($endtime - $starttime, 3) . '秒<br>';
 echo '消耗内存: ' . memory_get_usage() / (1024 * 1024) . 'MB<br />';
